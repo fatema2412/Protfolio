90d263d046e3140950cac38ae846a33cc73ef1ec
Fatema Menjarwala <fatemamenjarwala1623@gmail.com>
2025-03-01 23:37:22 +0530

    temp

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+const resumePdf = "/Protfolio/FatemaMenjarwala-FrontendDeveloper(React)-Ln4O.pdf";
+
+
+
+
+
+
+// const aboutImage=`${import.meta.env.BASE_URL}/assets/home/aboutImage.png`;
+const aboutImage = `${import.meta.env.BASE_URL}/assets/home/frontend-developer.png`;
+
+export default function Home() {
+  return (
+    <div id ="home"className='Home'>
+      <img src={aboutImage} alt='Profile Img'/>
+      <h1><span>I'm Fatema Menjarwala </span>, Frontend Developer  </h1>
+      <p>I have completed my Frontend Development Course from Masai School, Bangalore</p>
+      <div className='home-action'>
+      <div className="hero-resume">
+  <a  style={{textDecoration:"none"}}href={resumePdf} download="Fatema_Menjarwala_Resume.pdf">
+ Resume  </a>
+ </div>
+
+        <div className="hero-connect"> <AnchorLink className='anchor-link' offset={50} href='#Contact'>Connect with Me </AnchorLink></div>
+        {/* <div className="hero-resume">My Resume</div> */}
+      </div>
+    </div>
+  )
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from 'react'
+import './Navbar.css'
+import { getImageUrl } from "../../utils";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+const closeIcon=`${import.meta.env.BASE_URL}/assets/nav/closeIcon.png`
+const menuIcon=`${import.meta.env.BASE_URL}/assets/nav/menuIcon.png`
+const heroImage = `${import.meta.env.BASE_URL}/assets/home/heroImage.png`
+const resumePdf = "/Protfolio/FatemaMenjarwala-FrontendDeveloper(React)-Ln4O.pdf";
+
+// import heroImage from "../assets/home/heroImage.png";
+
+
+export default function Navbar() {
+  const [menu,setMenu]=useState("home")
+  const menuRef=useRef()
+  const openMenu=()=>{
+    menuRef.current.style.right="0";
+  }
+  const closeMenu=()=>{
+    menuRef.current.style.right="-350px";
+  }
+  return (
+    <div className='navbar'>
+      <img src={heroImage} alt='logo'/>
+      <img src="https://img.icons8.com/?size=48&id=6uJdcB0tVRwZ&format=gif" onClick={openMenu} alt='menuIcone' className='nav-mob-open'/>
+      <ul ref={menuRef}  className="nav-menu">
+        <img src={closeIcon} onClick={closeMenu} alt="closeIcon" className="nav-mob-close" style={{backgroundColor:"blue"}} />
+        <li><AnchorLink className='anchor-link'  href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu=="home"? <img src="https://www.protaxaccounting.co.uk/wp-content/uploads/2015/03/Underline-5-Blue.png" style={{
+        display: "block",
+        width: "100%",
+        height: "50px",
+        marginTop: "-10px", // Adjust to bring it closer without shifting text
+      }}/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#AboutMe'><p onClick={()=>setMenu("AboutMe")}>About Me</p></AnchorLink>{menu=="About Me"? <img src="https://www.protaxaccounting.co.uk/wp-content/uploads/2015/03/Underline-5-Blue.png" style={{
+        display: "block",
+        width: "100%",
+        height: "50px",
+        marginTop: "-10px", // Adjust to bring it closer without shifting text
+      }}/>:<></>} </li>
+        <li><AnchorLink className='anchor-link'offset={50} href='#Skills'><p onClick={()=>setMenu("Skills")}>Skills</p></AnchorLink>{menu=="Skills"? <img src="https://www.protaxaccounting.co.uk/wp-content/uploads/2015/03/Underline-5-Blue.png" style={{
+        display: "block",
+        width: "100%",
+        height: "50px",
+        marginTop: "-10px", // Adjust to bring it closer without shifting text
+      }}/>:<></>}</li>
+        <li><AnchorLink className='anchor-link'offset={50} href='#Project'><p onClick={()=>setMenu("Project")}>Project</p></AnchorLink>{menu=="Project"? <img src="https://www.protaxaccounting.co.uk/wp-content/uploads/2015/03/Underline-5-Blue.png" style={{
+        display: "block",
+        width: "100%",
+        height: "50px",
+        marginTop: "-10px", // Adjust to bring it closer without shifting text
+      }}/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#Contact'><p onClick={()=>setMenu("Contact")}>Contact</p></AnchorLink>{menu=="Contact"? <img src="https://www.protaxaccounting.co.uk/wp-content/uploads/2015/03/Underline-5-Blue.png" style={{
+        display: "block",
+        width: "100%",
+        height: "50px",
+        marginTop: "-10px", // Adjust to bring it closer without shifting text
+      }}/>:<></>}</li>
+
+      </ul>
+      <div className="nav-connect">
+  <a href={resumePdf} download="Fatema_Menjarwala_Resume.pdf">
+ Resume  </a>
+  </div>
+  </div>
+
+  )
+}
