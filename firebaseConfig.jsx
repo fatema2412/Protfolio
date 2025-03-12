84a0de41aa2c6b7546c800e22df3b15148911a24
Fatema Menjarwala <fatemamenjarwala1623@gmail.com>
2025-03-13 04:16:32 +0530

    Fix gitignore

---
+++ firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAs6IHq2yyUYIPPTjjQPQ4Z8z_a5X-hCZI",
+  authDomain: "protfolio-f8a84.firebaseapp.com",
+  projectId: "protfolio-f8a84",
+  storageBucket: "protfolio-f8a84.firebasestorage.app",
+  messagingSenderId: "396309795631",
+  appId: "1:396309795631:web:5284759a0089836c7787da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
